148f3cdb3cdf392f9dd56b27fde2efcf50093bcb
etzJohn <holman.jx@gmail.com>
2019-08-02 15:54:18 -0700

    v1

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-const contactLink = document.querySelector('.landing.contact');
-const email = document.querySelector('.landing.contact_email');
-const linkedIn = document.querySelector('.landing.contact_linkedin');
 const aboutLink = document.querySelector('.landing.about');
 const portfolioLink = document.querySelector('.landing.portfolio');
 const github = document.querySelector('.landing.portfolio_github');
-
-if (contactLink && email && linkedIn) {
-  contactLink.addEventListener('click', () => {
-    contactLink.innerText = `
-      xavi.labs@protonmail.com | linkedIn
-    `;
-  });
-}
 
 if (aboutLink) {
   aboutLink.addEventListener('click', () => {
@@ -20,14 +9,6 @@ if (aboutLink) {
       and creating exceptional user experiences.
       Proven track record of delivering quality web applications at scale.
       Passionate about culinary, inclusion, and napping in parks :)
-    `;
-  });
-}
-
-if (portfolioLink && github) {
-  portfolioLink.addEventListener('click', () => {
-    portfolioLink.innerText = `
-      GitHub
     `;
   });
 }
